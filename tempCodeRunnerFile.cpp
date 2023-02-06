3e031cfc4a4a90d1b0d3f281ce5440d83a7699d7
Mosseir <glock18_123@hotmail.com>
2023-02-06 20:39:28 +0700

    Done

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+        // for(int i = 0 ;i < N;i++){
+        //     sum += A[i];
+        //     sum4 = sum4 + A[i];
+        //     sum2 += pow(A[i],2);
+        //     sum3 = sum3 * A[i];
+        //     // if(A[i]>max) max = A[i];
+        //     // if(A[i]<min) min = A[i];
+            
+        // }
